9d95b4203ffea6ef19dcdbd89d96d4fa8dcbf545
Pouria <pnsadeghy@gmail.com>
2023-03-08 21:58:56 +0330

    MainMap component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainMapComponent } from './components/map/main-map/main-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMapComponent
   ],
   imports: [
     BrowserModule
