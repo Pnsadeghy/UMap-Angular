b57c46536eceb94866b16ee7397d7f4b00991e9d
Pouria <pnsadeghy@gmail.com>
2023-03-08 22:16:50 +0330

    Install ngx-leaflet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {LeafletModule} from "@asymmetrik/ngx-leaflet";
 
 import { AppComponent } from './app.component';
 import { MainMapComponent } from './components/map/main-map/main-map.component';
@@ -10,7 +11,8 @@ import { MainMapComponent } from './components/map/main-map/main-map.component';
     MainMapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
