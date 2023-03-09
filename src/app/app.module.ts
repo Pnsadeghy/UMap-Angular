12f6f31232223fd394964456e7e45e5a15a40e68
Pouria <pnsadeghy@gmail.com>
2023-03-09 10:26:14 +0330

    add localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { BrowserModule } from '@angular/platform-browser';
 import {LeafletModule} from "@asymmetrik/ngx-leaflet";
 import {NgbModalModule} from "@ng-bootstrap/ng-bootstrap";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
 import { AppComponent } from './app.component';
 import { MainMapComponent } from './components/map/main-map/main-map.component';
 import { UserNewButtonComponent } from './components/users/user-new-button/user-new-button.component';
 import { UserFormComponent } from './components/users/user-form/user-form.component';
 import { UserModalComponent } from './components/users/user-modal/user-modal.component';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -20,7 +27,15 @@ import { UserModalComponent } from './components/users/user-modal/user-modal.com
   imports: [
     BrowserModule,
     LeafletModule,
-    NgbModalModule
+    NgbModalModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
