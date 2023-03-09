f585dd526193ead552d49dbdd197beaa7b08f087
Pouria <pnsadeghy@gmail.com>
2023-03-09 09:19:46 +0330

    Add scss New button css

--- src/app/components/users/user-new-button/user-new-button.component.ts
+++ src/app/components/users/user-new-button/user-new-button.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-new-button',
   templateUrl: './user-new-button.component.html',
-  styleUrls: ['./user-new-button.component.css']
+  styleUrls: ['./user-new-button.component.scss']
 })
 export class UserNewButtonComponent {
 
