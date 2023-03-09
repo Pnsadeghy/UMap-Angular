06c4144d81769b04455f4d0fbe54e84b29db7772
Pouria <pnsadeghy@gmail.com>
2023-03-09 08:47:03 +0330

    Used real map in map component

--- src/app/components/map/main-map/main-map.component.ts
+++ src/app/components/map/main-map/main-map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {latLng, tileLayer} from "leaflet";
 
 @Component({
   selector: 'app-main-map',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-map.component.css']
 })
 export class MainMapComponent {
-
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
 }
